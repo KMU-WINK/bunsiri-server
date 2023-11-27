ff9a63e095d6f059984f28255a0acdabbe220290
yoon2000 <yoonhs02@kookmin.ac.kr>
2023-11-27 11:15:21 +0900

    feature/messageAPI

--- nodejs/controllers/messageController.js
+++ nodejs/controllers/messageController.js
@@ -1,33 +1,55 @@
-// 필요한 패키지나 모듈을 불러오는 부분
+const messageService = require("../services/messageService");
 
-// 메시지 목록 조회 컨트롤러
-const getAllMessages = (req, res) => {
-  // 여기에 메시지 목록을 조회하는 로직을 추가합니다.
-  res.send("Get all messages");
+const getAllMessages = async (req, res) => {
+  try {
+    const messages = await messageService.getAllMessages();
+    res.send(messages);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
-// 메시지 상세 정보 조회 컨트롤러
-const getMessageById = (req, res) => {
-  // 여기에 메시지 상세 정보를 조회하는 로직을 추가합니다.
-  res.send("Get message by ID");
+const getMessageById = async (req, res) => {
+  try {
+    const messageId = req.params.id;
+    const message = await messageService.getMessageById(messageId);
+    res.send(message);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
-// 메시지 생성 컨트롤러
-const createMessage = (req, res) => {
-  // 여기에 메시지를 생성하는 로직을 추가합니다.
-  res.send("Create message");
+const createMessage = async (req, res) => {
+  try {
+    const createdMessage = await messageService.createMessage(req.body);
+    res.send(createdMessage);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
-// 메시지 업데이트 컨트롤러
-const updateMessage = (req, res) => {
-  // 여기에 메시지 정보를 업데이트하는 로직을 추가합니다.
-  res.send("Update message");
+const updateMessage = async (req, res) => {
+  try {
+    const messageId = req.params.id;
+    const updatedMessage = await messageService.updateMessage(
+      messageId,
+      req.body
+    );
+    res.send(updatedMessage);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
-// 메시지 삭제 컨트롤러
-const deleteMessage = (req, res) => {
-  // 여기에 메시지를 삭제하는 로직을 추가합니다.
-  res.send("Delete message");
+const deleteMessage = async (req, res) => {
+  try {
+    const messageId = req.params.id;
+    const result = await messageService.deleteMessage(messageId);
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
 };
 
 module.exports = {
